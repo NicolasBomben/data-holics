f828df42ac4ed8a3fee60bcdbb5e185b58912449
BombenDev <nicolasbomben@gmail.com>
2023-06-26 19:23:32 -0300

    working on solutions

--- src/pages/Solutions.jsx
+++ src/pages/Solutions.jsx
@@ -1,8 +1,69 @@
 import React from 'react'
 
+import "./solutions.css";
+
 const Solutions = () => {
   return (
-    <div>Solutions</div>
+    <section className="solutions">
+      <div className="solutions-main">
+        <h1 className="solutions-main-text">Solutions</h1>
+      </div>
+      <div className="solutions-secondary">
+        <p className="secondary-text">
+          Analytics & Artificial intelligence for your business.
+        </p>
+        <p className="secondary-text">
+          Unsleash the power of your data whit Machine Learning and Analytics.
+        </p>
+      </div>
+      <div className="solutions-container">
+        <p className="box-text">Basic Analytics</p>
+        <div className="solutions-box">
+          
+          <div className="box-img">
+
+          </div>
+          <ul className="box-list">
+            <li className="box-item">
+              Dignosis.
+            </li>
+            <li className="box-item">
+              Data Strategy and pipelines.
+            </li>
+            <li className="box-item">
+              Storeo
+            </li>
+            <li className="box-item">
+              Estrategia de almacenamiento y gestion de datos.
+            </li>
+            <li className="box-item">
+              Major Kpis related to business.
+            </li>
+            <li className="box-item">
+              Desarrollo de Kpis relevantes al caso de negocio.
+            </li>
+            <li className="box-item">
+              Principales clientes/productos.
+            </li>
+            <li className="box-item">
+              Personalized dashboards using your data.
+            </li>
+            <li className="box-item">
+              Usando la informacion previa, desarrollo de dashboards accesibles por distintas fuentes.
+            </li>
+            <li className="box-item">
+              Financial data and industry-related reports(Montly).
+            </li>
+            <li className="box-item">
+              Actualizacion mensual de datos financieros relevantes para el negcio utilizando licencia de bloomerg + CHATGPT.
+            </li>
+          </ul>
+        </div>
+
+
+
+      </div>
+    </section>
   )
 }
 
